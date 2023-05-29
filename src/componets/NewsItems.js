438981c83d2f3eb319dffc9e9aa1ed19e9734ada
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-05-29 14:50:32 +0530

    implementing local json file for testing purpose

---
+++ src/componets/NewsItems.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class NewsItems extends Component {
+    render () {
+        let { title, desc, imageUrl, newsUrl } = this.props;
+        return (
+            <div className="card" style={{ width: "18rem" }}>
+                <img src={imageUrl} className="card-img-top" style={{ width: "100%", height: "10rem" }} alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{desc}</p>
+                    <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary">Read</a>
+                </div>
+            </div>
+        )
+    }
+}
