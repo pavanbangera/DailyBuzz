cfe5eb29bc86b27214afbe7affdd77d5b0922900
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-05-30 18:30:01 +0530

    implimenting scroling pagination with loading spinner and top loading bar

--- src/App.js
+++ src/App.js
@@ -9,22 +9,33 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
+  state = {
+    progress: 0
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
   render () {
     return (
       <div >
         <Router>
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+          />
           <NavBar />
           <Routes>
-            <Route exact path="/" element={<News key="1" pageSize={6} category="general" heading="Home" />}></Route>
-            <Route exact path="/business" element={<News key="2" pageSize={6} category="business" heading="business" />}></Route>
-            <Route exact path="/entertainment" element={<News key="3" pageSize={6} category="entertainment" heading="entertainment" />}></Route>
-            <Route exact path="/general" element={<News key="4" pageSize={6} category="general" heading="general" />}></Route>
-            <Route exact path="/health" element={<News key="5" pageSize={6} category="health" heading="health" />}></Route>
-            <Route exact path="/science" element={<News key="6" pageSize={6} category="science" heading="science" />}></Route>
-            <Route exact path="/sports" element={<News key="7" pageSize={6} category="sports" heading="sports" />}></Route>
-            <Route exact path="/technology" element={<News key="8" pageSize={6} category="technology" heading="technology" />}></Route>
+            <Route exact path="/" element={<News setProgress={this.setProgress} key="1" pageSize={6} category="general" heading="Home" />}></Route>
+            <Route exact path="/business" element={<News setProgress={this.setProgress} key="2" pageSize={6} category="business" heading="business" />}></Route>
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="3" pageSize={6} category="entertainment" heading="entertainment" />}></Route>
+            <Route exact path="/general" element={<News setProgress={this.setProgress} key="4" pageSize={6} category="general" heading="general" />}></Route>
+            <Route exact path="/health" element={<News setProgress={this.setProgress} key="5" pageSize={6} category="health" heading="health" />}></Route>
+            <Route exact path="/science" element={<News setProgress={this.setProgress} key="6" pageSize={6} category="science" heading="science" />}></Route>
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} key="7" pageSize={6} category="sports" heading="sports" />}></Route>
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} key="8" pageSize={6} category="technology" heading="technology" />}></Route>
 
           </Routes>
 
--- src/componets/News.js
+++ src/componets/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 import SpinnerBar from './SpinnerBar'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
     constructor () {
@@ -8,61 +9,92 @@ export default class News extends Component {
         this.state = {
             articles: [],
             page: 1,
-            loading: false,
+            // loading: true,
 
         }
     }
-    updatePage = async () => {
+    // updatePage = async () => {
+    //     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=1483be2622ac49ff8dc53bf1c7b373f7&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    //     this.setState({ loading: true })
+    //     let data = await fetch(url);
+    //     let parseData = await data.json();
+    //     this.setState({
+    //         articles: parseData.articles,
+    //         totalResult: parseData.totalResults,
+    //         loading: false
+    //     })
+    // }
+    async componentDidMount () {
+        this.props.setProgress(10);
         let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=1483be2622ac49ff8dc53bf1c7b373f7&page=${this.state.page}&pageSize=${this.props.pageSize}`
-        this.setState({ loading: true })
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parseData = await data.json();
+        this.props.setProgress(60);
         this.setState({
             articles: parseData.articles,
             totalResult: parseData.totalResults,
-            loading: false
-        })
-    }
-    async componentDidMount () {
-        this.updatePage()
-    }
-    handlePrevClick = async () => {
-        await this.setState({
-            page: this.state.page - 1,
-        })
-        await this.updatePage()
+            page: this.state.page + 1,
 
+
+        })
+        this.props.setProgress(100);
     }
-    handleNextClick = async () => {
-        await this.setState({
+    // handlePrevClick = async () => {
+    //     await this.setState({
+    //         page: this.state.page - 1,
+    //     })
+    //     await this.updatePage()
+
+    // }
+    // handleNextClick = async () => {
+    //     await this.setState({
+    //         page: this.state.page + 1,
+    //     })
+    //     await this.updatePage()
+    // }
+    fetchMoreData = async () => {
+
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=1483be2622ac49ff8dc53bf1c7b373f7&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles),
+            totalResult: parseData.totalResults,
             page: this.state.page + 1,
         })
-        await this.updatePage()
     }
     render () {
         return (
 
-            <div className='container my-3'>
+            <>
                 <h2 className='text-center'>DailyBuzz - Top Headlines [ {this.props.heading}] </h2>
-                {this.state.loading && <SpinnerBar />}
-                <div className="row justify-content-center">
-                    {
 
-                        !this.state.loading && this.state.articles.map((e) => {
-                            console.log(e)
-                            return <div key={e.url} className="col-3 mx-4 my-3">
-                                <NewsItems title={e.title ? e.title.slice(0, 45) : "(no title)"} desc={e.description ? e.description.slice(0, 90) : "(no description)"} imageUrl={e.urlToImage ? e.urlToImage : "https://i.ytimg.com/vi/1Zv6peOteRY/maxresdefault.jpg"} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name} />
-                            </div>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResult}
+                    loader={<SpinnerBar />}
 
-                        })
-                    }
+                ><div className="container"> <div className="row justify-content-center"> {
 
-                </div>
-                <div className="container d-flex justify-content-between">
+
+                    this.state.articles.map((e) => {
+                        console.log(e)
+                        return <div key={e.url} className="col-3 mx-4 my-3">
+                            <NewsItems title={e.title ? e.title.slice(0, 45) : "(no title)"} desc={e.description ? e.description.slice(0, 90) : "(no description)"} imageUrl={e.urlToImage ? e.urlToImage : "https://i.ytimg.com/vi/1Zv6peOteRY/maxresdefault.jpg"} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name} />
+                        </div>
+
+                    })
+                } </div>
+                    </div></InfiniteScroll>
+
+
+                {/* <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} class="btn btn-primary" onClick={this.handlePrevClick}>&larr; Preview</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / 18)} class="btn btn-primary" id="next" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-            </div>
+                </div> */}
+            </>
         )
     }
 }
